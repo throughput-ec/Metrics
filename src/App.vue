b58501ee4cb67fadba875ab097d93054e00d7116
Simon <simon.j.goring@gmail.com>
2019-11-22 19:26:18 -0800

    Adding Vue components and dist folder for the project build.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"  class="mainpage">
+    <img alt="Throughput logo" src="./assets/throughput.svg">
+    <pPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import './assets/text.css';
+import './assets/containers.css'
+
+import pPage from './components/parentPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    pPage
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
