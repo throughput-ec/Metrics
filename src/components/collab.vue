55ab1cb3744f29a6778b6d4a6eb18e548c0432f6
Simon <simon.j.goring@gmail.com>
2020-05-19 22:32:16 -0700

    Removing all the node_modules.

--- src/components/collab.vue
+++ src/components/collab.vue
@@ -1,22 +1,37 @@
 <template>
   <div>
-    <div class="componentbox">
-      <h2>Throughput Collaboration</h2>
-      <div class="componentbox">
-        <h3>Core Concepts</h3>
-        Throughput is, fundamentally, a database that links several core data objects.
-        These objects include:
-        <ul>
-        <li>Objects</li>
-        <li>Annotations</li>
-        <li>Types</li>
-        <li>Language</li>
-        <li>XXXX</li>
-        </ul>
-        <h3>Adding New Resources</h3>
-        <h3>Bug Reporting</h3>
-      </div>
-    </div>
+    <b-card title="Throughput Collaboration">
+      <b-card title="Core Concepts">
+        <b-card-text>
+          Throughput is, fundamentally, a database that links several core data objects.
+          These objects include:
+          <ul>
+          <li>Objects</li>
+          <li>Annotations</li>
+          <li>Types</li>
+          <li>Language</li>
+          <li>XXXX</li>
+          </ul>
+        </b-card-text>
+      </b-card>
+      <b-card title="Adding New Resources" />
+      <b-card title="Bug Reporting" />
+      <b-card title="Goal">
+        <b-card-text>
+          Building user interfaces to support user groups within the earth sciences including Direct Annotation, The Earth Science Cookbook
+        </b-card-text>
+        <b-card title="Metrics">
+          <b-card-text>
+            <ul>
+              <li>Direct annotation landing page built and tagged in a version control system (GitHub), with Usability report from the UMN Usability Lab.</li>
+              <li>Documentation surrounding the usability report.</li>
+              <li>Earth Science Cookbook UI built and tagged in version control system.</li>
+              <li>Usability report from the Usability Lab.</li>
+            </ul>
+          </b-card-text>
+        </b-card>
+      </b-card>
+    </b-card>
   </div>
 </template>
 
---
+++ src/components/earthscicook.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-card title="Workflow Discovery">
+      <b-card title="Goal">
+        <b-card-text>
+          Establishing a platform to support the addition of workflow elements
+          to the TAE and to link primary data resources in the geosciences
+          through publicly available code.
+        </b-card-text>
+        <b-card title="Metrics">
+          <b-card-text>
+            <ul>
+              <li>Number of scripts recovered by programmatic workflows</li>
+              <li>Number of user-submitted scripts</li>
+              <li>Number of data resources connected through scripts</li>
+              <li>Number of users accessing scripts through the system</li>
+              <li>Number of workshops (and number of participants) that use annotation as an element in instruction.</li>
+            </ul>
+          </b-card-text>
+        </b-card>
+      </b-card>
+    </b-card>
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'ECcook',
+    props: {
+      msg: String
+    }
+  }
+
+</script>
